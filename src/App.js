a3774dd6456569029b285ceb0a081d9662e46a9e
dcryptoniun <dcryptoniun@gmail.com>
2023-03-12 13:22:17 +0530

    starter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import "./App.css";
-
-import { useState, useEffect } from "react";
+import React from "react";
+import Footer from "./component/Footer";
+import Navebar from "./component/Navbar";
+import Home from "./Home";
 
 function App() {
-  const [theme, setTheme] = useState(null);
-
-  useEffect(() => {
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-      setTheme('dark');
-    }
-    else {
-      setTheme('light');
-    }
-  }, [])
-
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [theme]);
-
-  const handleThemeSwitch = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
   return (
-    <div className="h-screen bg-white dark:bg-black flex justify-center items-center">
-      <button className="bg-green-200 p-4 rounded-3xl" onClick={handleThemeSwitch}>
-        Dark Mode
-      </button>
+    <div className="w-screen h-screen text-black dark:text-white flex flex-col bg-[#ECF2FF] dark:bg-[#0f172a]">
+      <Navebar />
+      <Home />
+      <Footer />
     </div>
   );
 }
